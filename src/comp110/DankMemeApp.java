a018c961c2511d455149168ea9447e81b1cc7f86
Kris Jordan <krisjordan@gmail.com>
2017-01-14 17:58:08 -0500

    Initial setup for PS0 of Spring 2017.

---
+++ src/comp110/DankMemeApp.java
@@ -0,0 +1,21 @@
+package comp110;
+
+/*
+ * Author: <YOUR NAME>
+ *
+ * ONYEN: <YOUR ONYEN>
+ *
+ * UNC Honor Pledge: I certify that no unauthorized assistance has been 
+ * received or given in the completion of this work. I collaborated with
+ * no one other than official COMP110 UTAs on this code.
+ */
+public class DankMemeApp {
+
+  public static void main(String[] args) {
+
+    // Your code will go below this line. You can delete this line and the next.
+    // These two lines are "comments" because they start with two slashes.
+
+  }
+
+}
